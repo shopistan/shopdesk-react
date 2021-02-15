9e101a2cf5d26a7c13804e691623f09f6990b937
rafay <abdul.rafay@shopdev.co>
2021-02-15 15:08:38 +0500

    fix: menu links added

--- src/components/pages/appShell/index.js
+++ src/components/pages/appShell/index.js
@@ -35,12 +35,12 @@ const AppShell = (props) => {
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo">
+        <div className='logo'>
           <h2>Shopdesk</h2>
         </div>
         <Menu
-          theme="dark"
-          mode="inline"
+          theme='dark'
+          mode='inline'
           defaultSelectedKeys={["dashboard"]}
           onClick={(e) => {
             if (e.key === "dashboard") {
@@ -51,60 +51,66 @@ const AppShell = (props) => {
               history.push("/signup");
             } else if (e.key === "signin") {
               history.push("/signin");
-            } 
+            }
           }}
         >
-          <Menu.Item key="dashboard" icon={<DashboardOutlined />}>
+          <Menu.Item key='dashboard' icon={<DashboardOutlined />}>
             Dashboard
           </Menu.Item>
-          <Menu.Item key="categories" icon={<TagsOutlined />}>
+          <Menu.Item key='categories' icon={<TagsOutlined />}>
             Categories
           </Menu.Item>
-          <Menu.Item key="signup" icon={<SendOutlined />}>
+          <Menu.Item key='3' icon={<SendOutlined />}>
             Suppliers
           </Menu.Item>
-          <Menu.Item key="signin" icon={<BankOutlined />}>
+          <Menu.Item key='4' icon={<BankOutlined />}>
             Taxes
           </Menu.Item>
-          <Menu.Item key="5" icon={<ShopOutlined />}>
+          <Menu.Item key='5' icon={<ShopOutlined />}>
             Products
           </Menu.Item>
-          <Menu.Item key="6" icon={<UserOutlined />}>
+          <Menu.Item key='6' icon={<UserOutlined />}>
             Customers
           </Menu.Item>
-          <Menu.Item key="7" icon={<BarcodeOutlined />}>
+          <Menu.Item key='7' icon={<BarcodeOutlined />}>
             Couriers
           </Menu.Item>
-          <SubMenu key="sub1" icon={<LaptopOutlined />} title="Register">
-            <Menu.Item key="8">Sell</Menu.Item>
-            <Menu.Item key="9">Sales History</Menu.Item>
+          <SubMenu key='sub1' icon={<LaptopOutlined />} title='Register'>
+            <Menu.Item key='8'>Sell</Menu.Item>
+            <Menu.Item key='9'>Sales History</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" icon={<StockOutlined />} title="Stock Control">
-            <Menu.Item key="10">Purchase Orders</Menu.Item>
-            <Menu.Item key="11">Inventory Transfers</Menu.Item>
-            <Menu.Item key="12">Stock Adjustment</Menu.Item>
+          <SubMenu key='sub2' icon={<StockOutlined />} title='Stock Control'>
+            <Menu.Item key='10'>Purchase Orders</Menu.Item>
+            <Menu.Item key='11'>Inventory Transfers</Menu.Item>
+            <Menu.Item key='12'>Stock Adjustment</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub3" icon={<ApartmentOutlined />} title="Ecommerce">
-            <Menu.Item key="13">Orders</Menu.Item>
-            <Menu.Item key="14">Inventory Sync</Menu.Item>
+          <SubMenu key='sub3' icon={<ApartmentOutlined />} title='Ecommerce'>
+            <Menu.Item key='13'>Orders</Menu.Item>
+            <Menu.Item key='14'>Inventory Sync</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub4" icon={<BarChartOutlined />} title="Reports">
-            <Menu.Item key="14">Sales Summary</Menu.Item>
-            <Menu.Item key="16">Inventory Dump</Menu.Item>
-            <Menu.Item key="17">Product History</Menu.Item>
-            <Menu.Item key="18">Omni Sales Summary</Menu.Item>
-            <Menu.Item key="19">Category Wise</Menu.Item>
+          <SubMenu key='sub4' icon={<BarChartOutlined />} title='Reports'>
+            <Menu.Item key='14'>Sales Summary</Menu.Item>
+            <Menu.Item key='16'>Inventory Dump</Menu.Item>
+            <Menu.Item key='17'>Product History</Menu.Item>
+            <Menu.Item key='18'>Omni Sales Summary</Menu.Item>
+            <Menu.Item key='19'>Category Wise</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub5" icon={<SettingOutlined />} title="Setup">
-            <Menu.Item key="20">Outlets</Menu.Item>
-            <Menu.Item key="21">Users</Menu.Item>
-            <Menu.Item key="22">Receipt Templates</Menu.Item>
+          <SubMenu key='sub5' icon={<SettingOutlined />} title='Setup'>
+            <Menu.Item key='20'>Outlets</Menu.Item>
+            <Menu.Item key='21'>Users</Menu.Item>
+            <Menu.Item key='22'>Receipt Templates</Menu.Item>
           </SubMenu>
+          <Menu.Item key='signup' icon={<SendOutlined />}>
+            Sign Up
+          </Menu.Item>
+          <Menu.Item key='signin' icon={<BankOutlined />}>
+            Sign In
+          </Menu.Item>
         </Menu>
       </Sider>
-      <Layout className="site-layout">
-        <Header className="header site-layout-background">
-          <div className="header__menu-btn">
+      <Layout className='site-layout'>
+        <Header className='header site-layout-background'>
+          <div className='header__menu-btn'>
             {React.createElement(
               collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
               {
@@ -114,20 +120,16 @@ const AppShell = (props) => {
             )}
           </div>
 
-          <div className="header__content">
-            <div className="outlet">
+          <div className='header__content'>
+            <div className='outlet'>
               <h2>Outlet Name</h2>
             </div>
-            <div className="user">
+            <div className='user'>
               <h2>Username</h2>
             </div>
           </div>
         </Header>
-        <Content
-          className="site-layout-background"
-        >
-          {props.children}
-        </Content>
+        <Content className='site-layout-background'>{props.children}</Content>
       </Layout>
     </Layout>
   );
