5240e4fde5b3e37e2ee22dab9788e6e806a546c9
rafay <abdul.rafay@shopdev.co>
2021-02-17 14:36:43 +0500

    feat: tax page ui

---
+++ src/components/pages/taxes/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { Button, Select, Input } from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
+import EditableTable from "../../organism/table";
+
+const Taxes = () => {
+  const { Option } = Select;
+
+  const { Search } = Input;
+
+  const onSearch = (value) => console.log(value);
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+
+  return (
+    <div className='page categories'>
+      <div className='page__header'>
+        <h2>Taxes</h2>
+        <Button type='primary' icon={<PlusCircleOutlined />}>
+          Add New
+        </Button>
+      </div>
+      <div className='page__content'>
+        <div className='action-row'>
+          <div className='action-row__element'>
+            Show
+            <Select
+              defaultValue='10'
+              style={{ width: 120, margin: "0 5px" }}
+              onChange={handleChange}
+            >
+              <Option value='25'>25</Option>
+              <Option value='50'>50</Option>
+              <Option value='100'>100</Option>
+            </Select>
+            entries
+          </div>
+
+          <div className='action-row__element'>
+            <Search
+              placeholder='search category'
+              allowClear
+              enterButton='Search'
+              size='large'
+              onSearch={onSearch}
+            />
+          </div>
+        </div>
+
+        {/* Table */}
+        <div className='table'>
+          <EditableTable />
+        </div>
+        {/* Table */}
+      </div>
+    </div>
+  );
+};
+
+export default Taxes;
