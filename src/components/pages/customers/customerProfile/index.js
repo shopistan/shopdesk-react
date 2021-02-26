dbf23b5c17c35880eecb091cbb607331897f7342
Rafay <abdul.rafay@shopdev.co>
2021-02-26 10:24:49 +0500

    feat: test commit

---
+++ src/components/pages/customers/customerProfile/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomerProfile = () => {
+  return (
+    <div className="page customer-profile">
+      <h1>Customer Profile</h1>
+    </div>
+  );
+};
+
+export default CustomerProfile;
