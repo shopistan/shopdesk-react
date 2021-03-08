6b7c55ddbd869c37b6d844acf306af76f8a75138
Muzzammil Jamil <muzzammil.jamil@shopdev.co>
2021-03-08 09:25:58 +0500

    commit for inventory task of reporting module.

---
+++ src/components/organism/table/reports/productsInventoryTable.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import { Table } from "antd";
+
+
+const ProductsInventoryTable = (props) => {
+  const [data, setData] = useState([]);
+
+
+  const showTotalItemsBar = (total, range) => {
+    console.log(range);
+    return `${range[0]}-${range[1]} of ${total} items`
+  };
+  
+
+  useEffect(() => {
+    setData(props.tableData);
+
+  }, [props.tableData, props.tableDataLoading]);  /* imp passing props to re-render */
+
+
+  var columns;
+
+
+
+    columns = [
+      {
+        title: "SKU",
+        dataIndex: "sku",
+      },
+      {
+        title: "Name",
+        dataIndex: "name",
+      },
+      {
+        title: "Variant 1",
+        dataIndex: "quantity",
+      },
+      {
+        title: "Variant 2",
+        dataIndex: "variant2",
+      },
+      {
+        title: "Quantity",
+        dataIndex: "variant2",
+      },
+      {
+        title: "Stock Value",
+        dataIndex: "stock_value",
+        render: (_, record) => {
+            return (
+              <span> {parseFloat(record.stock_value).toFixed(2)} </span>
+            );
+          },
+      },
+      {
+        title: "Item Value",
+        dataIndex: "item_value",
+        render: (_, record) => {
+            return (
+              <span> {parseFloat(record.item_value).toFixed(2)} </span>
+            );
+          },
+      },
+      
+      
+  
+    ];
+  
+
+  
+
+
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+
+        dataindex: col.dataIndex,
+        title: col.title,
+      }),
+    };
+  });
+
+
+
+  return (
+
+    <Table
+      bordered
+      dataSource={data}
+      columns={mergedColumns}
+      rowClassName='editable-row'
+      className='table-frame'
+      id={props.tableId} //imp to pass table id here
+      pagination={{
+        total: data && data.length,
+        showTotal: (total, range) => showTotalItemsBar(total, range),
+        defaultPageSize: 20,
+        pageSize: parseInt(props.pageLimit),
+        showSizeChanger: false,
+
+      }}
+      loading={props.tableDataLoading}
+      
+    />
+  );
+};
+
+export default ProductsInventoryTable;
+
--- src/components/pages/reports/inventoryDump/index.js
+++ src/components/pages/reports/inventoryDump/index.js
@@ -1,34 +1,139 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style.scss";
 
-import { Button, Badge } from "antd";
+import { Button, message, Divider } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
+import ProductsInventoryTable from "../../../organism/table/reports/productsInventoryTable";
+import * as ReportsApiUtil from '../../../../utils/api/reports-api-utils';
 
 const InventoryDump = () => {
-  const [show, setShow] = React.useState(true);
+  const [inventoryCount, SetinventoryCount] = useState("");
+  const [inventoryData, setInventoryData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+
+  const fetchProductsInventoryData = async () => {
+
+    const productsInventoryResponse = await ReportsApiUtil.viewPrductsInventory();
+    console.log('productsInventoryResponse Response:', productsInventoryResponse);
+
+    if (productsInventoryResponse.hasError) {
+      console.log('Cant fetch Products Inventory Data -> ', productsInventoryResponse.errorMessage);
+      message.error(productsInventoryResponse.errorMessage, 3);
+      setLoading(false);
+    }
+    else {
+      console.log('res -> ', productsInventoryResponse);
+      message.success(productsInventoryResponse.message, 3);
+      setInventoryData(productsInventoryResponse.inventory_report);
+      setLoading(false);
+      SetinventoryCount((productsInventoryResponse.inventory_report).length);
+
+
+    }
+  }
+
+
+
+  useEffect(() => {
+    fetchProductsInventoryData();
+
+  }, []);
+
+
+
+  function download_csv(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV FILE
+    csvFile = new Blob([csv], { type: "text/csv" });
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // We have to create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Make sure that the link is not displayed
+    downloadLink.style.display = "none";
+
+    // Add the link to your DOM
+    document.body.appendChild(downloadLink);
+
+    // Lanzamos
+    downloadLink.click();
+  }
+
+
+  function export_table_to_csv(html, filename) {
+    var csv = [];
+    //imp selection below
+    var rows = document.querySelectorAll("div#products_inventory_data_table tr");
+
+    for (var i = 0; i < rows.length; i++) {
+      var row = [], cols = rows[i].querySelectorAll("td, th");
+
+      for (var j = 0; j < cols.length; j++)
+        row.push(cols[j].innerText);
+
+      csv.push(row.join(","));
+    }
+
+    // Download CSV
+    download_csv(csv.join("\n"), filename);
+  }
+
+
+  const DownloadToCsv = (e) => {
+
+    if (inventoryData.length > 0) {
+      var html = document.getElementById('products_inventory_data_table').innerHTML;
+
+      export_table_to_csv(html, "sales_summary.csv");
+
+    }
+    else { message.error("No Sales Data Found", 3) }
+
+  }
+
+
 
   return (
-    <div className='page reports'>
+    <div className='page reports-inventory'>
       <div className='page__header'>
         <h1>Inventory Dump</h1>
 
-        <Button type='primary' icon={<DownloadOutlined />}>
+        <Button type='primary'
+          icon={<DownloadOutlined />}
+          onClick={DownloadToCsv}
+        >
           Download
         </Button>
       </div>
 
       <div className='page__content'>
         <div className='action-row'>
-          <Badge
-            className='site-badge-count-109'
-            count={show ? 109 : 0}
-            style={{ backgroundColor: "#52c41a" }}
-          />
+          <strong className="inventory-count-badge">Total: {inventoryCount}</strong>
         </div>
 
-        <div className='page__table'>{/* Insert Table Here */}</div>
+        <div className='table inventory-table'>{/* Insert Table Here */}
+          <div className='form__section__header'>
+            <h3 className='variants-heading'>Inventory Dump</h3>
+          </div>
+          
+          <ProductsInventoryTable tableId='products_inventory_data_table' pageLimit={20} tableData={inventoryData}
+            tableDataLoading={loading} />
+        </div>
+
       </div>
+
     </div>
+
   );
 };
 
