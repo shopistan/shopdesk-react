7cc353c3d11b3d157b3eef5fd69853930edd8449
abdullahamin0111 <abdullah.amin.0111@gmail.com>
2021-03-15 13:23:52 +0500

    feat: added links for customer and invoice number

--- src/components/pages/customers/customerCreditHistory/index.js
+++ src/components/pages/customers/customerCreditHistory/index.js
@@ -111,22 +111,6 @@ const CustomerCreditHistory = (props) => {
             );
           })}
 
-          <VerticalTimelineElement
-            className='vertical-timeline-element--work'
-            contentStyle={{ background: '#f5f5f6', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  black' }}
-            date='2011 - present'
-            iconStyle={{ background: '#f5f5f6', color: '#fff' }}
-          >
-            <h3 className='vertical-timeline-element-title'>
-              Creative Director
-            </h3>
-            <h4 className='vertical-timeline-element-subtitle'>Miami, FL</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, Project
-              Management, Team Leading
-            </p>
-          </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
     </div>
