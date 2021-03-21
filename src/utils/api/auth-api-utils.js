b63229578b67c570b40ed4e88feeee2b9efdbf76
abdullahamin0111 <abdullah.amin.0111@gmail.com>
2021-03-22 01:08:16 +0500

    feat: authentication flow setup

--- src/utils/api/auth-api-utils.js
+++ src/utils/api/auth-api-utils.js
@@ -1,38 +1,22 @@
-import axios from 'axios';
+import GenericConstants from '../constants/constants';
 import UrlConstants from '../constants/url-configs';
+import * as ApiCallUtil from './generic-api-utils';
 
 export const login = async (userNameOrEmail, passsword) => {
-  const loginFormData = new FormData();
-  loginFormData.append('username', userNameOrEmail);
-  loginFormData.append('password', passsword);
+  const formDataPair = {
+    username: userNameOrEmail,
+    password: passsword
+  };
 
-  try {
-    const loginCall = await axios({
-      method: 'post',
-      url: UrlConstants.AUTH.LOGIN,
-      data: loginFormData
-    });
+  const loginFormData = ApiCallUtil.constructFormData(formDataPair);
+  const url = UrlConstants.AUTH.LOGIN;
+  const callType = GenericConstants.API_CALL_TYPE.POST;
 
-    const loginResponse = loginCall.data;
-    console.log('login response is: ', loginResponse);
-
-    if (loginResponse && loginResponse.status) {
-      return {
-        hasError: false,
-        data: loginResponse
-      };
-    }
-    return {
-      hasError: true,
-      errorMessage: loginResponse.messsage || 'Invalid Credentials'
-    };
-  } catch (err) {
-    console.log('unable to login due to ', err);
-    return {
-      hasError: true,
-      errorMessage: 'Invalid Credentials'
-    };
-  }
+  return await ApiCallUtil.http(
+    url, //api url
+    callType, //calltype
+    loginFormData //body
+  );
 };
 
 export const signUp = async (
@@ -44,42 +28,25 @@ export const signUp = async (
   businessName,
   businessPassword
 ) => {
-  const signUpFormData = new FormData();
-  signUpFormData.append('fname', firstName);
-  signUpFormData.append('email', email);
-  signUpFormData.append('password', password);
-  signUpFormData.append('repassword', confirmPassword);
-  signUpFormData.append('phone', phone);
-  signUpFormData.append('businessName', businessName);
-  signUpFormData.append('businessAddress', businessPassword);
+  const formDataPair = {
+    fname: firstName,
+    email: email,
+    password: password,
+    repassword: confirmPassword,
+    phone: phone,
+    businessName: businessName,
+    businessAddress: businessPassword
+  };
 
-  try {
-    const signUpCall = await axios({
-      method: 'post',
-      url: UrlConstants.AUTH.SIGNUP,
-      data: signUpFormData
-    });
+  const signUpFormData = ApiCallUtil.constructFormData(formDataPair);
+  const url = UrlConstants.AUTH.SIGNUP;
+  const callType = GenericConstants.API_CALL_TYPE.POST;
 
-    const signUpResponse = signUpCall.data;
-    console.log('sign up response is: ', signUpResponse);
-
-    if (signUpResponse && signUpResponse.status) {
-      return {
-        hasError: false,
-        data: signUpResponse
-      };
-    }
-    return {
-      hasError: true,
-      errorMessage: signUpResponse.messsage || 'Invalid Credentials'
-    };
-  } catch (err) {
-    console.log('unable to sign up due to ', err);
-    return {
-      hasError: true,
-      errorMessage: 'Invalid Credentials'
-    };
-  }
+  return await ApiCallUtil.http(
+    url, //api url
+    callType, //calltype
+    signUpFormData //body
+  );
 };
 
 // const login = async (userNameOrEmail, passsword) => {
--- src/utils/local-storage/local-store-utils.js
+++ src/utils/local-storage/local-store-utils.js
@@ -1,6 +1,9 @@
 import Constants from '../../utils/constants/constants';
 import moment from 'moment';
 
+export const clearLocalUserData = () => {
+  localStorage.removeItem(Constants.USER_AUTH_KEY);
+};
 
 export const saveDataIntoLocalStorage = (key, value) => {
   try {
@@ -29,28 +32,25 @@ export const clearDataFromLocalStorage = () => {
   localStorage.clear();
 };
 
-
 export const checkUserAuthFromLocalStorage = (key) => {
   const dataFromLocalStorage = JSON.parse(window.localStorage.getItem(key));
   return {
-    authentication: dataFromLocalStorage.hasOwnProperty(Constants.USER_AUTH_KEY),
+    authentication: dataFromLocalStorage.hasOwnProperty(Constants.USER_AUTH_KEY)
   };
 };
-
 
 export const checkAuthTokenExpiration = (expirationDate) => {
   var currentDate = new Date();
   var authExpirationTokenDate;
-  currentDate = moment(currentDate).format("yyyy-MM-DD HH:mm");
-  authExpirationTokenDate = moment(expirationDate).format("yyyy-MM-DD HH:mm");
-    if( currentDate >  authExpirationTokenDate){
-      clearDataFromLocalStorage();
-      return true;
-    }
-    else { return false; }
-
-}
-
+  currentDate = moment(currentDate).format('yyyy-MM-DD HH:mm');
+  authExpirationTokenDate = moment(expirationDate).format('yyyy-MM-DD HH:mm');
+  if (currentDate > authExpirationTokenDate) {
+    clearDataFromLocalStorage();
+    return true;
+  } else {
+    return false;
+  }
+};
 
 export const getSellInvoiceDataFromLocalStorage = (key) => {
   const dataFromLocalStorage = JSON.parse(window.localStorage.getItem(key));
